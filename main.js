71e0aae8fea46ee9b9b506a56c2bbd0ce5483b0a
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2019-05-10 16:26:02 +0900

    タブソート機能を実装

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
+var util = {
+  windows: {
+    getAll() {
+      return new Promise(resolve => {
+        chrome.windows.getAll(null, (windows) => {
+          resolve(windows);
+        });
+      });
+    },
+
+    getCurrent() {
+      return new Promise(r => chrome.windows.getCurrent(null, w => r(w)));
+    },
+  },
+
+  tabs: {
+    getAll(windowId) {
+      return new Promise(r => chrome.tabs.getAllInWindow(windowId, e => r(e)));
+    },
+
+    getCurrent() {
+      return new Promise(r => chrome.tabs.getCurrent(e => r(e)));
+    },
+
+    async sort(desc = false) {
+      var n = desc ? 1 : -1;
+      var tabs = [];
+      (await util.windows.getAll()).forEach(async (w) => {
+        var _tabs = await util.tabs.getAll(w.id);
+        _tabs = await new Promise(r => chrome.tabs.move(_tabs.sort((a, b) => a.url < b.url ? n : -n).map(e => e.id), { index: -1 }, tabs => r(tabs)));
+        tabs = tabs.concat(_tabs);
+      });
+      return tabs;
+    },
+  },
+
+};
+
+
 riot.mount('app');
--- tags.js
+++ tags.js
@@ -1,2 +1,11 @@
-riot.tag2('app', '<h1>hello</h1>', 'app,[data-is="app"]{display:block;max-width:800px}', '', function(opts) {
+riot.tag2('app', '<button class="button primary" ref="sort" type="button" onclick="{sortTabs}">タブソート</button>', 'app,[data-is="app"]{display:block;width:800px;max-width:800px}', '', function(opts) {
+
+    this.on('mount', () => {
+      this.refs.sort.focus();
+    });
+
+    this.sortTabs = async () => {
+      await util.tabs.sort();
+      close();
+    };
 });
