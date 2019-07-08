1e75e7feaf8d38a08b24ad063b5afeba7dbf8089
simiraaaa <c7t2h8m3y0@yahoo.co.jp>
2019-07-08 16:41:41 +0900

    bugfix

--- tags.js
+++ tags.js
@@ -8,6 +8,33 @@ riot.tag2('app', '<div class="f flex-column s-full"> <module-tab-switcher class=
       util.close();
     };
 });
+riot.tag2('item-domain', '<div class="f fm w-full p4 border-bottom cursor-pointer {opts.selected ? \'bg-link text-white\' : \'hover-bg-primary hover-text-white\'}"> <div class="mr4 flex-fixed s20 f fh bg-white rounded-4"> <img riot-src="{opts.item.favIconUrl}" alt="" class="object-fit-cover s16"> </div> <div> <div class="line-clamp-2 word-break-all white-space-pre-wrap w-full fs12 lh12">{opts.item.name}</div> </div> </div>', 'item-domain,[data-is="item-domain"]{display:block}', '', function(opts) {
+    this.on('updated', () => {
+      if (!this._lastSelected && opts.selected) {
+        this.root.scrollIntoView({
+          block: 'nearest',
+          inline: 'nearest',
+        });
+      }
+      this._lastSelected = opts.selected;
+    });
+
+});
+riot.tag2('item-tab', '<div class="f fm w-full p4 border-bottom cursor-pointer {opts.selected ? \'bg-link text-white\' : \'hover-bg-primary hover-text-white\'}"> <div class="mr4 flex-fixed s24 f fh bg-white rounded-4"> <img riot-src="{opts.item.favIconUrl}" alt="" class="object-fit-cover s20"> </div> <div> <div class="line-clamp-1 word-break-all white-space-pre-wrap w-full fs12 lh12">{opts.item.title}</div> <div class="line-clamp-1 word-break-all white-space-pre-wrap w-full fs10 parent-hover-text-white lh12 {opts.selected ? \'text-white\' : \'text-weak\'}">{opts.item.url}</div> </div> </div>', 'item-tab,[data-is="item-tab"]{display:block}', '', function(opts) {
+    this.on('mount', () => {
+      this.update();
+    });
+    this.on('updated', () => {
+      if (!this._lastSelected && opts.selected) {
+        this.root.scrollIntoView({
+          block: 'nearest',
+          inline: 'nearest',
+        });
+      }
+      this._lastSelected = opts.selected;
+    });
+
+});
 riot.tag2('module-tab-switcher', '<form onsubmit="{submit}" class="f flex-column s-full" onkeydown="{shortcut}" onmousemove="{onmousemove}"> <input ref="search" type="search" oninput="{delaySearch}" class="input w-full fs12 flex-fixed letter-spacing-1"> <div tabindex="-1" class="outline-none f w-full overflow-hidden"> <div class="flex-fixed w200 h-full f flex-column border-right"> <div class="bg-lightgray w-full p6 fs12 text-center flex-fixed">Shift + ↓↑</div> <div class="s-full overflow-scroll"> <item-domain each="{item in items}" item="{item}" selected="{_currentFocusDomain === item}" onclick="{parent.focusTabFirst}"></item-domain> </div> </div> <div class="s-full f flex-column"> <div class="bg-lightgray w-full p6 fs12 text-center flex-fixed">↓↑</div> <div class="overflow-scroll s-full" ref="scroll"> <div each="{domain in items}"> <div class="bg-whitesmoke border-bottom w-full py8 px6 fs12 flex-fixed">{domain.name}</div> <item-tab each="{item in domain.tabs}" item="{item}" onclick="{switchTab}" selected="{item.__index === parent.parent.selectIndex}" onmouseenter="{mouseenter}" class="{\'opacity-50 pointer-none\' : item._del}"></item-tab> </div> </div> </div> </div> </form>', 'module-tab-switcher,[data-is="module-tab-switcher"]{display:block}', '', function(opts) {
 
     this.selectIndex = 0;
@@ -255,29 +282,5 @@ riot.tag2('module-tab-switcher', '<form onsubmit="{submit}" class="f flex-column
       this._lastHover = e.item.item;
       e.preventUpdate = true;
     };
-
-});
-riot.tag2('item-tab', '<div class="f fm w-full p4 border-bottom cursor-pointer {opts.selected ? \'bg-link text-white\' : \'hover-bg-primary hover-text-white\'}"> <div class="mr4 flex-fixed s24 f fh bg-white rounded-4"> <img riot-src="{opts.item.favIconUrl}" alt="" class="object-fit-cover s20"> </div> <div> <div class="line-clamp-1 word-break-all white-space-pre-wrap w-full fs12 lh12">{opts.item.title}</div> <div class="line-clamp-1 word-break-all white-space-pre-wrap w-full fs10 parent-hover-text-white lh12 {opts.selected ? \'text-white\' : \'text-weak\'}">{opts.item.url}</div> </div> </div>', 'item-tab,[data-is="item-tab"]{display:block}', '', function(opts) {
-    this.on('updated', () => {
-      if (!this._lastSelected && opts.selected) {
-        this.root.scrollIntoView({
-          block: 'nearest',
-          inline: 'nearest',
-        });
-      }
-      this._lastSelected = opts.selected;
-    });
-
-});
-riot.tag2('item-domain', '<div class="f fm w-full p4 border-bottom cursor-pointer {opts.selected ? \'bg-link text-white\' : \'hover-bg-primary hover-text-white\'}"> <div class="mr4 flex-fixed s20 f fh bg-white rounded-4"> <img riot-src="{opts.item.favIconUrl}" alt="" class="object-fit-cover s16"> </div> <div> <div class="line-clamp-2 word-break-all white-space-pre-wrap w-full fs12 lh12">{opts.item.name}</div> </div> </div>', 'item-domain,[data-is="item-domain"]{display:block}', '', function(opts) {
-    this.on('updated', () => {
-      if (!this._lastSelected && opts.selected) {
-        this.root.scrollIntoView({
-          block: 'nearest',
-          inline: 'nearest',
-        });
-      }
-      this._lastSelected = opts.selected;
-    });
 
 });
